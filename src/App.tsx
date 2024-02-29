b8f1f902c6be2e295d7a6160cdf1e0a07a968e08
alanhe <qianghe421@gmail.com>
2024-02-29 23:40:35 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Mousetrap from 'mousetrap'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    const mousetrap = Mousetrap.bind("mod+k", function() { alert('show shortcuts!'); });
+    return ()=>{
+      mousetrap.reset();
+    }
+  },[]);
   return (
     <>
       <div>
